2f9c48606029337d077d3be35aff5531d4404bf6
deterquetejoder <saecularisator@gmail.com>
2023-07-21 01:21:20 +0500

    turn all into English, works

--- example005_ifelse/Program.cs
+++ example005_ifelse/Program.cs
@@ -1,1 +1,15 @@
 ﻿
+const string prompt = "Insert username: ";
+const string masha = "Maria";
+
+Console.Write(prompt);
+string username = Console.ReadLine();
+
+if(username == masha)
+{
+    Console.Write("Yike! It's Maria!");
+}
+else
+{
+    Console.Write("Hello, "+username);
+}
