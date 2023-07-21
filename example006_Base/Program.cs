b7b115baab42e4e987bf1824a35d3b90fc455df5
deterquetejoder <saecularisator@gmail.com>
2023-07-21 19:19:49 +0500

    max in list

--- example006_Base/Program.cs
+++ example006_Base/Program.cs
@@ -1,7 +1,6 @@
 ﻿List<int> numbers = new List<int> { 1, 2, 6, 8, 4 };
 numbers.Sort();
 
-foreach (int number in numbers)
-{
-    Console.WriteLine(number);
-}
+int max = numbers[numbers.Count - 1];
+
+Console.WriteLine(max);
